0294ae0ccddd749485724531ec1b677d816f4b79
Chuck D'Antonio <chuck@crdant.io>
2026-04-18 05:41:43 -0400

    chore: bumps version to 0.3.0 for encrypted transport

--- Sources/dug/Dug.swift
+++ Sources/dug/Dug.swift
@@ -2,7 +2,7 @@ import ArgumentParser
 import Foundation
 
 /// Application version — referenced by CLI --version and output headers.
-let dugVersion = "0.2.1"
+let dugVersion = "0.3.0"
 
 @main
 struct Dug: AsyncParsableCommand {
